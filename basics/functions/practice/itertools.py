9ec5560ce4ff5d7e236252c1c2fe82e94ccddce0
Арсений Соколовский <as@vestasystems.local>
2025-08-25 17:56:53 +0300

    Itertools

--- basics/functions/practice/itertools.py
+++ basics/functions/practice/itertools.py
@@ -1,6 +1,8 @@
 import itertools
+import re
+import sympy
 
-#Task 1: Lock combinations
+# Task 1: Lock combinations
 
 def lock_combinations():
     all_combinations = itertools.permutations(range(10), 4)
@@ -9,11 +11,25 @@ def lock_combinations():
     return list(suitable_combinations)
 
 
-#Task 2: Text analysis
+# Task 2: Text analysis
 
-def analyze_text(text):
-    all_pairs = itertools.product(text)
-    print(list(all_pairs))
+def analyze_text(text: str):
+    all_words = re.findall(r'\w+', text)
+    all_pairs = itertools.pairwise(all_words)
+    suitable_pairs = filter(lambda x: len(x[0]) == len(x[1]) and x[0].lower() != x[1].lower(), all_pairs)
+    return list(suitable_pairs)
+
+    
+# Task 3: 
+
+def dice_combinations(dice_n: int, dice_edges: int):
+    all_combinations = itertools.combinations(range(1, dice_edges + 1), dice_n)
+    good_combinations = []
+    for comb in all_combinations:
+        if len(set(comb)) == len(comb) and sympy.isprime(sum(comb)):
+            good_combinations.append(comb)
+    
+    return good_combinations
 
 
-analyze_text('Data science requires statistics and programming. Machine learning uses algorithms and models. Neural networks have layers and neurons. Gradient descent optimizes loss functions. Training data must be clean and representative. Validation helps prevent overfitting.')
+print(dice_combinations(3, 6))
